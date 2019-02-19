907a60d8bceb4450df59e461ad63dd326a2b01ee
Yuuya <rsa14759@gmail.com>
2019-02-20 03:21:45 +0900

    homework20 update

--- homework20/js/homework20.js
+++ homework20/js/homework20.js
@@ -1,13 +1,11 @@
-function clickEvent() {
-	$.ajax({
-		url: "http://192.168.33.10:4567/homework20",
-		data: $("#text").val(),
-		success: function(result) {
-			$("#result").html(result);
-		}
-	});
-}
-
 $(function() {
-	$("#submit").click(clickEvent());
+	$("#submit").click(function() {
+		$.ajax({
+			url: "http://192.168.33.10:4567/homework20",
+			data: $("#text").val(),
+			success: function(result) {
+				$("#result").html(result);
+			}
+		});
+	});
 });
