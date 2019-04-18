13af913771afc5495dbbff9ea7efe7751afd8079
Yuuya <rsa14759@gmail.com>
2019-04-18 15:11:18 +0000

    update(改修)
    ツイート取得時の条件からcreated_atを削除し、tweetsのidのみに修正

--- rails/twitterApp/app/models/user.rb
+++ rails/twitterApp/app/models/user.rb
@@ -12,14 +12,10 @@ end
 class FolloweeTweet < ActiveRecord::Base
   def self.find(param = {})
     param[:limit] = 30
-    if !(param.key?(:created_at))
-      param[:created_at] = Time.now
+    if !(param.key?(:tweets_id))
+      param[:tweets_id] = Tweet.maximum('id') + 1
     end
-    if param.key?(:tweets_id)
-      User.find_by_sql(['SELECT * FROM (SELECT "users".* FROM "users" INNER JOIN "relationships" ON "users"."id" = "relationships"."followee_id" WHERE "relationships"."follower_id" = :follower_id) AS "followee_user" INNER JOIN "tweets" ON "followee_user"."id" = "tweets"."user_id" WHERE "tweets"."id" < :tweets_id ORDER BY "tweets"."created_at" DESC LIMIT :limit', param])
-    else
-      User.find_by_sql(['SELECT * FROM (SELECT "users".* FROM "users" INNER JOIN "relationships" ON "users"."id" = "relationships"."followee_id" WHERE "relationships"."follower_id" = :follower_id) AS "followee_user" INNER JOIN "tweets" ON "followee_user"."id" = "tweets"."user_id" WHERE "tweets"."created_at" < :created_at ORDER BY "tweets"."created_at" DESC LIMIT :limit', param])
-    end
+    User.find_by_sql(['SELECT * FROM (SELECT "users".* FROM "users" INNER JOIN "relationships" ON "users"."id" = "relationships"."followee_id" WHERE "relationships"."follower_id" = :follower_id) AS "followee_user" INNER JOIN "tweets" ON "followee_user"."id" = "tweets"."user_id" WHERE "tweets"."id" < :tweets_id ORDER BY "tweets"."created_at" DESC LIMIT :limit', param])
   end
   
   def self.count_new_tweets(param)
