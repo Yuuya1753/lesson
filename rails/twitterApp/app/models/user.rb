e83075d54664be3fa98db2d75769a06f0a451764
Yuuya <rsa14759@gmail.com>
2019-04-20 14:03:49 +0000

    update(改修)
    ツイート取得のクエリを一つに集約

--- rails/twitterApp/app/models/user.rb
+++ rails/twitterApp/app/models/user.rb
@@ -11,18 +11,22 @@ end
 
 class FolloweeTweet < ActiveRecord::Base
   def self.find(param = {})
-    param[:limit] = 30
+    if !(param.key?(:limit))
+      param[:limit] = 30
+    end
     if !(param.key?(:tweets_id))
       param[:tweets_id] = Tweet.maximum('id') + 1
     end
-    User.find_by_sql(['SELECT * FROM (SELECT "users".* FROM "users" INNER JOIN "relationships" ON "users"."id" = "relationships"."followee_id" WHERE "relationships"."follower_id" = :follower_id) AS "followee_user" INNER JOIN "tweets" ON "followee_user"."id" = "tweets"."user_id" WHERE "tweets"."id" < :tweets_id ORDER BY "tweets"."created_at" DESC LIMIT :limit', param])
+
+    query_first_half = 'SELECT * FROM (SELECT "users".* FROM "users" INNER JOIN "relationships" ON "users"."id" = "relationships"."followee_id" WHERE "relationships"."follower_id" = :follower_id) AS "followee_user" INNER JOIN "tweets" ON "followee_user"."id" = "tweets"."user_id" WHERE "tweets"."id"'
+    query_last_half = ':tweets_id ORDER BY "tweets"."created_at" DESC LIMIT :limit'
+
+    query = query_first_half + param[:operator] + query_last_half
+    
+    User.find_by_sql([query, param])
   end
   
   def self.count_new_tweets(param)
     User.find_by_sql(['SELECT COUNT(*) AS "count" FROM (SELECT "users".* FROM "users" INNER JOIN "relationships" ON "users"."id" = "relationships"."followee_id" WHERE "relationships"."follower_id" = :follower_id) AS "followee_user" INNER JOIN "tweets" ON "followee_user"."id" = "tweets"."user_id" WHERE "tweets"."id" > :tweets_id', param])
-  end
-  
-  def self.find_new_tweets(param)
-    User.find_by_sql(['SELECT * FROM (SELECT "users".* FROM "users" INNER JOIN "relationships" ON "users"."id" = "relationships"."followee_id" WHERE "relationships"."follower_id" = :follower_id) AS "followee_user" INNER JOIN "tweets" ON "followee_user"."id" = "tweets"."user_id" WHERE "tweets"."id" > :tweets_id ORDER BY "tweets"."created_at" DESC LIMIT :limit', param])
   end
 end
