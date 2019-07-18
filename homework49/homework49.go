d1ed585af3654e21102cb3cc31967cebc985436f
丸山祐矢 <rsa14759@gmail.com>
2019-07-18 21:09:21 +0900

    homework49 update(引数Bが26を超えた場合の処理を追加)

--- homework49/homework49.go
+++ homework49/homework49.go
@@ -8,12 +8,19 @@ func main() {
 	fmt.Println(getString("abc", 3))
 	fmt.Println(getString("ace", 2))
 	fmt.Println(getString("xyz", 3))
+	fmt.Println(getString("xyz", 27))
+	fmt.Println(getString("xyz", -27))
 }
 
 func getString(a string, b int) string {
 	words := []rune(a)
 	size := len(words)
 	var result string
+	if b > 26 {
+		b -= 26
+	} else if b < -26 {
+		b += 26
+	}
 	for i := 0; i < size; i++ {
 		if int(words[i])+b > int('z') {
 			words[i] = rune((int(words[i]) + b) - 26)
